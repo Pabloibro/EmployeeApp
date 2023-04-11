9282b345ebb89fff497067e7d310a30b3bd1b1b8
Ogundipe ibrahim <ogundipeibrahim1111@gmail.com>
2023-04-11 11:14:23 +0200

    5 commit

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+def upload_to(instance, filename):
+   return 'post/{filename}'.format(filename=filename)
+
 class Employee(models.Model):
+   profile_picture = models.ImageField(_("Image"), upload_to=upload_to, default='post/ibro.jpg')
    full_name = models.CharField(max_length=200)
    email = models.CharField(max_length=100)
    contact= models.CharField(max_length=20)
